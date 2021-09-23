344ad312fede31c19b5722fb9dfd58d5fd2c02af
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2021-09-23 22:37:23 +0100

    First commit and setup

---
+++ src/test/java/com/edrumm/edrumm/EdrummApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edrumm.edrumm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdrummApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
