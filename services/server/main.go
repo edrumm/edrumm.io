82ed9e1d4a67a8732005f3b9cca8f8a53be31089
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2022-06-11 21:40:07 +0100

    Setup go server

---
+++ services/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "Connected",
+		})
+	})
+
+	/*
+	   ...
+	*/
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		if err := server.Close(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		if err != http.ErrServerClosed {
+			log.Fatal(err.Error())
+		}
+	}
+}
